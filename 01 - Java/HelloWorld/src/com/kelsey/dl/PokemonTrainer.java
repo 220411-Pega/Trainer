142eae3a493362f47b42ae0462803f77cd55b002
Jacob Davis <jacob.marquez.davis@hotmail.com>
2022-04-11 17:49:11 -0400

    Improved structure, uploading arrayws

---
+++ 01 - Java/HelloWorld/src/com/kelsey/dl/PokemonTrainer.java
@@ -0,0 +1,18 @@
+package com.kelsey.dl;
+
+public class PokemonTrainer {
+	
+	String firstName;
+	String lastName;
+	int age;
+	String typeFavorite;
+	int badges;
+	
+	@Override
+	public String toString() {
+		return "PokemonTrainer [firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + ", typeFavorite="
+				+ typeFavorite + ", badges=" + badges + "]";
+	}
+	
+}
+
